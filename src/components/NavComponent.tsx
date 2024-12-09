475f6595d005180dfc769237f71f141f8069df47
Amraleth <amraleth@proton.me>
2024-12-09 21:18:18 +0000

    Reformat nav

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -14,12 +14,12 @@ export default function NavComponent() {
                             <div class="flex space-x-4 pl-4">
                                 <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                                    aria-current="page">Karte</a>
-                                <a href="https://github.com/amraleth/sww"
-                                   class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                   target={"_blank"}>GitHub</a>
                                 <a href="https://api.schulwegwarnung.de/_/"
                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                    target={"_blank"}>Admin</a>
+                                <a href="https://github.com/amraleth/sww"
+                                   class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                   target={"_blank"}>GitHub</a>
                             </div>
                         </div>
                     </div>
