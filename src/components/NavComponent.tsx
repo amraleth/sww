05cf1c0ef373d7331ba3696c4f91fe1171dbbc43
Amraleth <amraleth@proton.me>
2024-12-09 21:15:43 +0000

    Add admin link to nav

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -17,6 +17,9 @@ export default function NavComponent() {
                                 <a href="https://github.com/amraleth/sww"
                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                    target={"_blank"}>GitHub</a>
+                                <a href="https://api.schulwegwarnung.de/_/"
+                                   class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                   target={"_blank"}>Admin</a>
                             </div>
                         </div>
                     </div>
