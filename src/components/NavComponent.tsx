7da47c958ef57d0e192062eb8b1954262ff1af90
Amraleth <amraleth@proton.me>
2024-12-13 09:20:42 +0000

    Block admin button on mobile

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -15,7 +15,7 @@ export default function NavComponent() {
                                 <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                                    aria-current="page">Karte</a>
                                 <a href="https://api.schulwegwarnung.de/_/"
-                                   class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                   class="hidden md:block rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                    target={"_blank"}>Admin</a>
                                 <a href="https://github.com/amraleth/sww"
                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
